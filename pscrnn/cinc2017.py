dfdb3592c9a0657b1b58a0b8534292710ee1bbd7
Daniel Schulman <daniel.schulman@philips.com>
2021-01-21 12:44:07 -0500

    cuda

--- pscrnn/cinc2017.py
+++ pscrnn/cinc2017.py
@@ -5,8 +5,9 @@ def run():
     dm = data.Cinc2017Data()
     m = classify.Classify(
         n_in = dm.N_IN, 
-        n_classes = dm.N_CLASSES)
-    trainer = pl.Trainer()
+        n_classes = dm.N_CLASSES,
+        n_hidden = 32)
+    trainer = pl.Trainer(gpus=1)
     trainer.fit(m, datamodule=dm)
 
 if __name__ == '__main__':
